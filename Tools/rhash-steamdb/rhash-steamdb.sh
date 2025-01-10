dfff072ebbe1918f6dc2efd8827b6974bf6a9f57
Yote.zip <118632916+YoteZip@users.noreply.github.com>
2025-01-10 07:57:37 -0600

    Code refactor for rhash manifest script - only cosmetic, no functional changes

--- Tools/rhash-steamdb/rhash-steamdb.sh
+++ Tools/rhash-steamdb/rhash-steamdb.sh
@@ -3,33 +3,31 @@
 # SteamDB-compatible RHash SHA1 manifest generator
 # For usage notes, run this script without arguments
 
-if ! [ -x "$(command -v rhash)" ]; then
-    echo "'rhash' program is required for SHA1 manifest generation"
-    exit 1
-elif ! [ -x "$(command -v sed)" ]; then
-    echo "'sed' program is required for SHA1 manifest generation"
-    exit 1
-fi
+(
+    for reqProg in "rhash" "sed"; do
+        if ! [[ -x "$(command -v "$reqProg")" ]]; then
+            echo "\"$reqProg\" program is required for execution"
+            exit 1
+        fi
+    done
 
-if [[ $# -eq 1 ]] && ([[ -d "$1" ]] || [[ -f "$1" ]]); then
-    CurRhashDir="$(pwd)"
-    hashfilename="$(basename "$(realpath "$1")")"
-    cd "$1" || return
-    rhash -H . -r -o "$CurRhashDir/$hashfilename.sha1"
-    sed -i -r -E 's/([0-9a-f]{40})  /\1 \*/g' "$CurRhashDir/$hashfilename.sha1"
-    cd - &> /dev/null || return
-elif [[ $# -eq 2 ]] && [[ -d "$1" ]]; then
-    CurRhashDir="$(pwd)"
-    cd "$1" || return
-    rhash -H . -r -o "$CurRhashDir/$2.sha1"
-    sed -i -r -E 's/([0-9a-f]{40})  /\1 \*/g' "$CurRhashDir/$2.sha1"
-    cd - &> /dev/null || return
-else
-    echo 'RHash hash generator:'
-    echo '-------------------------------------------------------------------------------'
-    echo 'Argument 1: Input folder'
-    echo 'Argument 2 (optional): Custom name for output hash file. (Default is file/folder name)'
-    echo '-------------------------------------------------------------------------------'
-fi
+    if [[ $# -ge 1 ]] && [[ -d "$1" ]]; then
+        if [[ $# -eq 1 ]]; then
+            hashfilename="$(basename "$(realpath "$1")")"
+        else
+            hashfilename="$2"
+        fi
 
-unset CurRhashDir hashfilename
+        CurRhashDir="$(pwd)"
+        cd "$1" || return
+
+        rhash -H . -r -o "$CurRhashDir/$hashfilename.sha1"
+        sed -i -r -E 's/([0-9a-f]{40})  /\1 \*/g' "$CurRhashDir/$hashfilename.sha1" # add a "*" before the filename, which is how SteamDB userscript wants the formatting
+    else
+        echo 'SteamDB-compatible RHash SHA1 manifest generator:'
+        echo '-------------------------------------------------------------------------------'
+        echo 'Argument 1: Input directory'
+        echo 'Argument 2 (optional): Custom name for output hash file. (Default is directory name)'
+        echo '-------------------------------------------------------------------------------'
+    fi
+)
