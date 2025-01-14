0414b8e4aab6abde495485044ac5648a63b4ef55
Yote.zip <118632916+YoteZip@users.noreply.github.com>
2025-01-14 11:35:05 -0600

    Tweak RHash manifest generator to natively format manifest correctly instead of cleaning up with sed

--- Tools/rhash-steamdb/rhash-steamdb.sh
+++ Tools/rhash-steamdb/rhash-steamdb.sh
@@ -3,12 +3,10 @@
 # SteamDB-compatible RHash SHA1 manifest generator
 # For usage notes, run this script without arguments
 
-for reqProg in "rhash" "sed"; do
-    if ! [[ -x "$(command -v "$reqProg")" ]]; then
-        echo "\"$reqProg\" program is required for execution"
-        exit 1
-    fi
-done
+if ! [[ -x "$(command -v "rhash")" ]]; then
+    echo "\"rhash\" program is required for execution"
+    exit 1
+fi
 
 if [[ $# -ge 1 ]] && [[ -d "$1" ]]; then
     if [[ $# -eq 1 ]]; then
@@ -20,8 +18,7 @@ if [[ $# -ge 1 ]] && [[ -d "$1" ]]; then
     CurRhashDir="$(pwd)"
     cd "$1" || return
 
-    rhash -H . -r -o "$CurRhashDir/$hashfilename.sha1"
-    sed -i -r -E 's/([0-9a-f]{40})  /\1 \*/g' "$CurRhashDir/$hashfilename.sha1" # add a "*" before the filename, which is how SteamDB userscript wants the formatting
+    rhash . --sha1 --recursive --output="$CurRhashDir/$hashfilename.sha1" --printf="%h *%p\n"
 else
     echo 'SteamDB-compatible RHash SHA1 manifest generator:'
     echo '-------------------------------------------------------------------------------'
